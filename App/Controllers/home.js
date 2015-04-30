0014254a6d6bc143207ca5f4325c66173545e15d
Javier <javigarcia@outlook.com>
2015-04-30 03:03:14 -0400

    Initial Commit
    Initial Commit

---
+++ App/Controllers/home.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    var homeController = function ($scope) {
+        $scope.appear = function () {
+            swal("Good job!", "You clicked the button!", "success")
+        }
+    };
+
+    homeController.$inject = ['$scope'];
+
+    angular.module('MyApplication')
+        .controller('HomeController', homeController);
+}());
